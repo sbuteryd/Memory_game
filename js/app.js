e5e82adf7306ca718cd882832b7ab46dfb45d24b
sbuteryd@qq.com <sbuteryd@qq.com>
2019-02-20 22:53:03 +0800

    version 1

--- js/app.js
+++ js/app.js
@@ -36,3 +36,46 @@ function shuffle(array) {
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
+
+
+//1 .deck 添加一个事件
+//2、ever.target.classList.tar()
+//
+
+const deck = document.querySelector('.deck');
+let second,third,list;
+
+function clickCard (event) {
+    console.log(event)
+    const first = document.querySelector('.open')//查看是否有打开元素，如果没有打开就打开
+    if (first === null) {
+        event.target.classList.add('open');
+        event.target.classList.add('show')
+        second = document.querySelector('.open').firstElementChild.classList.value.split()
+        list = document.querySelectorAll('.open')
+        //打开第二个元素
+        console.log(second)
+    }else if (second.length ===1 && list.length ===1){
+        event.target.classList.add('open');
+        event.target.classList.add('show');
+        third = event.target.querySelector('i').classList.value;
+        setTimeout(function () {
+           if (second[0] !== third){
+               event.target.className = 'card'//删除卡如果不匹配
+               const findOne = document.querySelector('.open');
+               findOne.className ='card'
+           }
+           if (second[0] === third) {//如果匹配 颜色统一
+               event.target.className = 'card match'//删除卡如果不匹配
+               const findOne = document.querySelector('.open');
+               findOne.className ='card match'
+           }
+
+        },1000)
+    }
+        list = document.querySelectorAll('.open')//查询第二个open 限制打开数
+
+}
+
+
+deck.addEventListener('click',clickCard)
